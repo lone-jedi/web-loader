d329de9a0d04d6ceb440578bc1ada57f30c14121
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-20 18:58:20 +0200

    Create ResourceReader & ResponseWriter

--- src/main/java/com/yarkin/server/resource/ResourceReader.java
+++ src/main/java/com/yarkin/server/resource/ResourceReader.java
@@ -1,4 +1,14 @@
 package com.yarkin.server.resource;
 
 public class ResourceReader {
+    public ResourceReader(String webAppPath, String uri) {
+    }
+
+    public boolean resourceExists() {
+        return false;
+    }
+
+    public String readResource(String uri) {
+        return null;
+    }
 }
--- src/main/java/com/yarkin/server/response/ResponseWritter.java
+++ src/main/java/com/yarkin/server/response/ResponseWritter.java
@@ -1,4 +1,23 @@
 package com.yarkin.server.response;
 
+import java.io.BufferedWriter;
+
 public class ResponseWritter {
+    public ResponseWritter(BufferedWriter writer) {
+    }
+
+    public String getBadRequestResponse(String bad_http_request) {
+        return null;
+    }
+
+    public void write(String response) {
+    }
+
+    public String getNotFoundResponse(String s) {
+        return null;
+    }
+
+    public String getSuccessResponse(String content) {
+        return null;
+    }
 }
