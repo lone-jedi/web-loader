a88154ec437532ed7106cb5e7de76af8b1f17e68
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-20 15:22:03 +0200

    Implements methods(without handle())

--- src/main/java/com/yarkin/server/request/RequestHandler.java
+++ src/main/java/com/yarkin/server/request/RequestHandler.java
@@ -1,4 +1,26 @@
 package com.yarkin.server.request;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
 public class RequestHandler {
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private String webAppPath;
+
+    public void setReader(BufferedReader reader) {
+        this.reader = reader;
+    }
+
+    public void setWriter(BufferedWriter writer) {
+        this.writer = writer;
+    }
+
+    public void setWebAppPath(String webAppPath) {
+        this.webAppPath = webAppPath;
+    }
+
+    public void handle() {
+        // processing...
+    }
 }
