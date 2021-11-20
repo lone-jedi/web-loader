434c97eb9a9647cbd6beca4adc1a272d03052c55
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-20 14:44:17 +0200

    Implements server methods (without start())

--- src/main/java/com/yarkin/server/Server.java
+++ src/main/java/com/yarkin/server/Server.java
@@ -1,4 +1,31 @@
 package com.yarkin.server;
 
 public class Server {
+    private static final int DEFAULT_PORT = 3000;
+    private int port;
+    private String webAppPath;
+
+    public void start() {
+        // server loop here ...
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        if(port < 0 || port > 65535) {
+            throw new IllegalArgumentException("The port \"" + port + "\" out of range [0, 65535]");
+        }
+
+        this.port = port;
+    }
+
+    public void setDefaultPort() {
+        setPort(DEFAULT_PORT);
+    }
+
+    public void setWebAppPath(String webAppPath) {
+        this.webAppPath = webAppPath;
+    }
 }
