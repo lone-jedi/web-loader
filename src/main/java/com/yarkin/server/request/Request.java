d8ac0655c06b2fcac56c8b7d86f34331734d4cb8
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-20 18:57:09 +0200

    Create Request Data class

--- src/main/java/com/yarkin/server/request/Request.java
+++ src/main/java/com/yarkin/server/request/Request.java
@@ -1,4 +1,53 @@
 package com.yarkin.server.request;
 
+import com.yarkin.server.HttpMethod;
+
+import java.util.HashMap;
+
 public class Request {
+    private HttpMethod httpMethod;
+    private String url;
+    private String httpVersion;
+    private HashMap<String, String> headers;
+    private String body;
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
+    public void setHttpMethod(HttpMethod httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
+    }
+
+    public void setHttpVersion(String httpVersion) {
+        this.httpVersion = httpVersion;
+    }
+
+    public HashMap<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(HashMap<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
