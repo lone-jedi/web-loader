c152dd3089ddedb4c3e5414cfbc10e8948feefb6
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-20 15:21:16 +0200

    Add tests for setWebAppPath & add check exceptions messages

--- src/test/java/com/yarkin/server/ServerTest.java
+++ src/test/java/com/yarkin/server/ServerTest.java
@@ -1,12 +1,20 @@
 package com.yarkin.server;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerTest {
     private Server server;
+
+    @BeforeAll
+    static void initialize() {
+        new File("src/main/test/resources/web-app").mkdirs();
+    }
 
     @BeforeEach
     public void before() {
@@ -22,17 +30,38 @@ public class ServerTest {
 
     @Test
     public void setServerPortLessThanZero() {
-        assertThrows(IllegalArgumentException.class, () -> server.setPort(-1));
+        assertThrows(IllegalArgumentException.class, () -> server.setPort(-1),
+                "The port \"-1\" out of range [0, 65535]");
     }
     
     @Test
     public void setServerPortWhichOutOfRange() {
-        assertThrows(IllegalArgumentException.class, () -> server.setPort(65535 + 1));
+        assertThrows(IllegalArgumentException.class, () -> server.setPort(65535 + 1),
+                "The port \"65536\" out of range [0, 65535]");
     }
 
     @Test
     public void setDefaultPort() {
         server.setDefaultPort();
         assertEquals(3000, server.getPort());
+    }
+
+    @Test
+    public void setExistsWebAppPath() {
+        assertDoesNotThrow(() -> server.setWebAppPath("src/main/test/resources/web-app"));
+        assertDoesNotThrow(() -> server.setWebAppPath("src/main/test/resources/web-app/"));
+        assertDoesNotThrow(() -> server.setWebAppPath("src/main/test/resources///web-app//"));
+    }
+
+    @Test
+    public void setNotExistsWebAppPath() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            server.setWebAppPath("src/main/test/resources/some-folder");
+        }, "Directory \"src/main/test/resources/some-folder\" does not exist");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            server.setWebAppPath("src/main/test/resources/web-app/some-folder");
+        }, "Directory \"src/main/test/resources/web-app/some-folder\" does not exist");
+
     }
 }
