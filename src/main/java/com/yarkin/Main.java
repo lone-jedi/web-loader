8e926f752f638c3a08013445bb9d16f43a7326e4
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-20 14:43:00 +0200

    Add usage of Server class

--- src/main/java/com/yarkin/Main.java
+++ src/main/java/com/yarkin/Main.java
@@ -1,7 +1,17 @@
 package com.yarkin;
 
+import com.yarkin.server.Server;
+
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server();
+        try {
+            server.setPort(3000);
+        } catch(IllegalArgumentException e) {
+            server.setDefaultPort();
+        }
 
+        server.setWebAppPath("src/main/resources/web-app");
+        server.start();
     }
 }
